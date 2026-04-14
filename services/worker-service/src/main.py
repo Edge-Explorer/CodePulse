d702dc5bb0f08ffdb39a06c9b86954114f9550e3
Karan Shelar <karanshelar8775@gmail.com>
2026-04-14 10:27:54 +0530

    feat(worker): connect scanner to kafka consumer loop
    Successfully integrated the AIScanner into the main consumer logic. The worker now performs end-to-end repository cloning and AI analysis upon receiving a message from Kafka.

--- services/worker-service/src/main.py
+++ services/worker-service/src/main.py
@@ -2,10 +2,14 @@ import asyncio
 import json
 import logging
 from aiokafka import AIOKafkaConsumer
+from src.scanner import AIScanner
+from src.config import settings
 
 logging.basicConfig(level= logging.INFO)
 
 async def consume():
+    # 1. Initialize the Scanner with the API KEY
+    scanner= AIScanner(api_key= settings.GEMINI_API_KEY)
     consumer= AIOKafkaConsumer(
         "project_scans",
         bootstrap_servers= '127.0.0.1:9092',
@@ -14,18 +18,25 @@ async def consume():
     )
 
     await consumer.start()
-    logging.info("AI Worker is listerning for task...")
+    logging.info("AI Worker is LIVE and listening for tasks...")
 
     try:
         async for msg in consumer:
             task= msg.value
             project_id= task.get("project_id")
-            repo= task.get("repo_url")
-            logging.info(f"RECEIVED TASK FOR PROJECT {project_id}: {repo}")
+            repo_url= task.get("repo_url")
+            logging.info(f"RECEIVED TASK FOR PROJECT {project_id}: {repo_url}")
 
-            logging.info(f"Scanning code in {repo}...")
-            await asyncio.sleep(5)
-            logging.info(f"SCAN COMPLETE for Project {project_id}!")
+            # 2. RUN THE REAL AI SCAN!
+            report= await scanner.scan_repo(repo_url)
+
+            if "error" in report:
+                logging.error(f"SCAN FAILED: {report['error']}")
+            else:
+                logging.info(f"SCAN COMPLETE for Project {project_id}!")
+                print("\n--- AI REPORT ---")
+                print(report["raw_report"])
+                print("-----------------\n")
 
     finally:
         await consumer.stop()
